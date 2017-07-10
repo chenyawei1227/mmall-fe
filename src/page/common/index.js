500dfffd2632b79e23c1b97290257587344bc01d
chenyawei1227 <cyw119900@gmail.com>
2017-07-10 20:29:26 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: chenyawei1227
+* @Date:   2017-07-09 21:54:58
+* @Last Modified by:   chenyawei1227
+* @Last Modified time: 2017-07-09 21:58:25
+*/
+
+'use strict';
+
+console.log('I am global');
---
+++ src/page/index/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: chenyawei1227
+* @Date:   2017-07-09 20:06:36
+* @Last Modified by:   chenyawei1227
+* @Last Modified time: 2017-07-09 22:13:32
+*/
+'use strict';
+require('./index.css');
+require('../module.js');
